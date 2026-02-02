2df56f8577593ba02159a9b76de97d2be729f83c
Jit Corn Leow <jleow6@gatech.edu>
2026-02-02 20:51:58 +0800

    add commentary on how the distributed lock works

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,8 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -11,6 +12,7 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	lockKey string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +23,48 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.lockKey = l
 	return lk
 }
 
+/**
+* Distributed Key Value Store Implementation
+* We store a lock "l" in the existing KV store using the Clerks via GET and PUT operations
+* It is a distributed lock because the lock is stored in a central location, but accessed by distributed clients
+* We perform a Compare and Swap operation to ensure that the lock is correctly populated.
+ */
+
 func (lk *Lock) Acquire() {
 	// Your code here
+	// Implementing something like a Compare-and-Swap (CAS)
+	// i.e comparing a memory location's current value to expected value and if its the same then we change the value
+	for {
+		val, version, err := lk.ck.Get(lk.lockKey)
+		if err == rpc.ErrNoKey {
+			if lk.ck.Put(lk.lockKey, "x", 0) == rpc.OK {
+				return
+			}
+		} else if val == "" {
+			if lk.ck.Put(lk.lockKey, "x", version) == rpc.OK {
+				return
+			}
+		}
+		// spin and retry in case there is any (rpc) network issues
+	}
+
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	for {
+		val, version, err := lk.ck.Get(lk.lockKey)
+		if err == rpc.ErrNoKey {
+			return
+		} else if val == "x" {
+			if lk.ck.Put(lk.lockKey, "", version) == rpc.OK {
+				return
+			}
+		}
+		// spin and retry in case there is rpc network issues
+	}
 }
